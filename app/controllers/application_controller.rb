f3e9e509b8f3b418dc4fb65439fa32d6ed073847
spreered <spreered@5xruby.tw>
2020-10-10 00:16:25 +0800

    add index page for normal user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(_user)
-    admin_root_path
+  def after_sign_in_path_for(user)
+    if user.admin?
+      admin_root_path
+    else
+      root_path
+    end
   end
 
   def after_sign_out_path_for(_user)

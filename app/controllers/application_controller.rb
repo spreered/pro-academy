7077e9e9d680bc3e3e4f83ac65b916e8af05cdf9
spreered <spreered@5xruby.tw>
2020-10-09 23:42:24 +0800

    modify user login redirect path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(_user)
+    admin_root_path
+  end
+
+  def after_sign_out_path_for(_user)
+    new_user_session_path
+  end
 end

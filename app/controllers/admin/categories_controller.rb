1a30fa3da7751d99e9e3c34247861c8631d0ede2
spreered <spreered@5xruby.tw>
2020-10-11 20:20:04 +0800

    category create

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,19 @@
 class Admin::CategoriesController < Admin::BaseController 
   def index
     @categories = Category.all
+    @new_category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    unless @category.save
+      flash[:alert] = "Failed to create category, reason: #{@category.errors.full_messages}"
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end

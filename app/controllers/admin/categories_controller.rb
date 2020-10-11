2b455e6486dea27588bf3f14e9d469982e20a603
spreered <spreered@5xruby.tw>
2020-10-11 22:51:23 +0800

    add category edit

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CategoriesController < Admin::BaseController 
   def index
-    @categories = Category.all
+    @categories = Category.all.order(id: :asc)
     @new_category = Category.new
   end
 
@@ -8,6 +8,17 @@ class Admin::CategoriesController < Admin::BaseController
     @category = Category.new(category_params)
     unless @category.save
       flash[:alert] = "Failed to create category, reason: #{@category.errors.full_messages}"
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    unless @category.update(category_params)
+      flash[:alert] = "Failed to update category, reason: #{@category.errors.full_messages}"
     end
   end
 

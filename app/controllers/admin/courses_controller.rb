c66ae9ccd3a7620a873ed03fa1d316248802c8bb
spreered <spreered@5xruby.tw>
2020-10-12 01:07:18 +0800

    add course show page

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -2,4 +2,9 @@ class Admin::CoursesController < Admin::BaseController
   def index
     @courses = Course.page(params[:page]).per(20).includes(:category)
   end
+
+  def show
+    @course = Course.find(params[:id])
+    @course_root_url = "#{request.base_url}/courses/"
+  end
 end

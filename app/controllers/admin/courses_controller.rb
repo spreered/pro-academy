daeabde6e5333f8c3b1793212ba84098fed434ac
spreered <spreered@5xruby.tw>
2020-10-13 00:42:49 +0800

    add course new

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -1,8 +1,22 @@
 class Admin::CoursesController < Admin::BaseController 
   before_action :find_course, only: [:show, :destroy, :edit, :update]
   def index
-    @courses = Course.order(id: :asc).page(params[:page]).per(20).includes(:category)
+    @courses = Course.order(id: :desc).page(params[:page]).per(20).includes(:category)
   end
+
+  def new
+    @course = Course.new
+  end
+
+  def create 
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to admin_course_url(@course)
+    else
+      flash.now[:alert] = "Create course failed, reason: #{@course.errors.full_messages}"
+      render :new
+    end
+  end 
 
   def show
     @course_root_url = "#{request.base_url}/courses/"
@@ -19,7 +33,7 @@ class Admin::CoursesController < Admin::BaseController
     if @course.update(course_params)
       redirect_to admin_course_url(@course)
     else
-      flash.now[:alert] = "update failed, reason: #{@course.errors.full_messages}"
+      flash.now[:alert] = "Update failed, reason: #{@course.errors.full_messages}"
       render :edit
     end
   end 

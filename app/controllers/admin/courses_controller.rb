e0d89b8e84e7d088b80a07dba749c6e29555fc70
spreered <spreered@5xruby.tw>
2020-10-12 22:36:00 +0800

    add course destroy

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -1,10 +1,23 @@
 class Admin::CoursesController < Admin::BaseController 
+  before_action :find_course, only: [:show, :destroy, :edit, :update]
   def index
-    @courses = Course.page(params[:page]).per(20).includes(:category)
+    @courses = Course.order(id: :asc).page(params[:page]).per(20).includes(:category)
   end
 
   def show
-    @course = Course.find(params[:id])
     @course_root_url = "#{request.base_url}/courses/"
+  end
+
+  def destroy
+    @course.destroy
+    flash[:notice] = "Delete course #{@course.id}: #{@course.title}"
+
+    redirect_to admin_root_url 
+  end
+
+  private
+
+  def find_course
+    @course = Course.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_08_155426) do
+ActiveRecord::Schema.define(version: 2020_10_12_140941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_10_08_155426) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description", default: ""
     t.index ["category_id"], name: "index_courses_on_category_id"
   end
 

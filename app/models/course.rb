cde0895bedef77156e67fbb1ffa3a9c9fe0ffaa6
spreered <spreered@5xruby.tw>
2020-10-13 00:59:41 +0800

    add course duration validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :category
   validates :title, :slug, presence: true, uniqueness: { case_sensitive: false, message: 'has been taken'}
+  validates :duration, inclusion: { in: (1..31) } 
   enum status: {delisted: 0, launched: 1}
 end

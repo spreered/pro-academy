f9698d8b5ae61743d1c6423d2da92e715db380a1
spreered <spreered@5xruby.tw>
2020-10-13 00:26:55 +0800

    add course edit

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -15,9 +15,22 @@ class Admin::CoursesController < Admin::BaseController
     redirect_to admin_root_url 
   end
 
+  def update
+    if @course.update(course_params)
+      redirect_to admin_course_url(@course)
+    else
+      flash.now[:alert] = "update failed, reason: #{@course.errors.full_messages}"
+      render :edit
+    end
+  end 
+
   private
 
   def find_course
     @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :category_id, :status, :slug, :duration, :description)
   end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ApplicationRecord
   belongs_to :category
+  validates :title, :slug, presence: true, uniqueness: { case_sensitive: false, message: 'has been taken'}
   enum status: {delisted: 0, launched: 1}
 end

473899a193eef551abc2dae09827e9267b90d9ee
spreered <spreered@5xruby.tw>
2020-10-16 11:34:43 +0800

    modify course api namesapce

---
+++ app/services/courses/checkout_order_service.rb
@@ -0,0 +1,5 @@
+module Courses
+  class ChectoutOrderService
+  end
+end
+
--- spec/api/courses_api/v1_spec.rb
+++ spec/api/courses_api/v1_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Courses::V1 do
+describe CoursesAPI::V1 do
   let(:user) { create(:user, :with_valid_access_token) }
   let(:access_token) { user.access_token }
   let(:headers) do 
